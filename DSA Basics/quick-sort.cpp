9f6fc6d8dcd404fad262814aa2489f9bfa24682d
Rahul Shaw <rahulramakrishana90@gmail.com>
2022-09-01 13:24:02 +0530

    changes in quick sort

--- DSA Basics/quick-sort.cpp
+++ DSA Basics/quick-sort.cpp
@@ -10,6 +10,12 @@ int partition(int l, int r, vector<int> &vec)
     {
         while (vec[l] <= vec[pivot])
         {
+            // because here a chance where l become greater than nums.size()-1 and access the illeagel space
+            // 54321 <- here l start from 0 index to last, and check nums[4] <= nums[0] and it become true, so l become 5 and then again check nums[5] <= nums[0], here it access illeagal space
+            if (l >= r)
+            {
+                break;
+            }
             ++l;
         }
         while (vec[r] > vec[pivot])
